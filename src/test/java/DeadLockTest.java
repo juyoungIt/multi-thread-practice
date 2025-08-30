a31f9ef1acdf39d643094c033c22a75d0f30c3e8
juyoungha <htj3460@gmail.com>
2025-08-31 07:26:16 +0900

    Deadlock 상황 재현 및 검증 테스트 코드 추가

---
+++ src/test/java/DeadLockTest.java
@@ -0,0 +1,105 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.management.ManagementFactory;
+import java.time.Duration;
+import java.util.*;
+import java.util.concurrent.CountDownLatch;
+
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class DeadLockTest {
+
+    private static final Object resource1 = new Object();
+    private static final Object resource2 = new Object();
+    CountDownLatch startGate;
+
+    @BeforeEach
+    void init() {
+        // 2개의 스레드를 서로 동시에 실행하기 위함
+        startGate = new CountDownLatch(1);
+    }
+
+    @Test
+    @DisplayName("Deadlock : 2개의 Thread가 서로의 자원을 기다리면서 Deadlock 이 발생한다")
+    void deadlockTest() {
+        // 실행시간이 3초가 넘어가는 경우 자동으로 종료하고 실패처리함
+        // -> 간단한 출력이기 때문에 deadlock이 발생하지 않는 이상 실행시간이 3초가 넘어갈 수 없음
+        assertTimeoutPreemptively(Duration.ofSeconds(3), () -> {
+
+            // 첫번째 Thread
+            // -> resource1을 먼저 획득한 뒤, resource2 획득을 시도함
+            Thread thread1 = new Thread(() -> {
+                synchronized (resource1) {
+                    System.out.println("Thread1: T1 acquired lock1");
+                    try {
+                        startGate.await();
+                        System.out.println("Thread1: T1 trying lock2...");
+                        synchronized (resource2) {
+                            System.out.println("Thread1: T1 acquired lock2");
+                        }
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }, "Thread1");
+
+            // 두번째 Thread
+            // -> resource2을 먼저 획득한 뒤, resource1 획득을 시도함
+            Thread thread2 = new Thread(() -> {
+                synchronized (resource2) {
+                    System.out.println("Thread2: T2 acquired lock2");
+                    try {
+                        startGate.await();
+                        System.out.println("Thread2: T2 trying lock1...");
+                        synchronized (resource1) {
+                            System.out.println("Thread2: T2 acquired lock1");
+                        }
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }, "Thread2");
+
+            thread1.start();
+            thread2.start();
+            startGate.countDown();
+
+            // 최대 1초 동안 DeadlockProbe로 교착 탐지 폴링
+            List<String> deadlockedNames = checkDeadlock(1000);
+            // 데드락에 걸린 스레드들을 검증
+            assertThat(deadlockedNames).containsExactlyInAnyOrder("Thread1", "Thread2");
+        });
+    }
+
+    /**
+     * 데드락 여부를 감지하여, Deadlock이 발생한 Thread의 이름을 List 형태로 반환
+     * @param timeoutMillis : 해당 시간동안 Polling 을 통해 Deadlock 발생여부를 감지
+     */
+    public static List<String> checkDeadlock(long timeoutMillis) throws InterruptedException {
+        long end = System.currentTimeMillis() + timeoutMillis;
+        while (System.currentTimeMillis() < end) {
+            long[] ids = ManagementFactory.getThreadMXBean().findDeadlockedThreads(); // deadlock 미검출 시 null 반환
+            if (ids != null) {
+                // 현재 실행 상태인 Thread ID, 이름 정보를 수집하여 Map으로 구성
+                Map<Long, String> threadNameMap = new HashMap<>();
+                for (Thread t : Thread.getAllStackTraces().keySet()) {
+                    threadNameMap.put(t.getId(), t.getName());
+                }
+                // 현재 Deadlock 상태에 빠진 Thread만 이름을 조회하여 List 로 구성
+                List<String> names = new ArrayList<>();
+                for (long id : ids) {
+                    String name = threadNameMap.get(id);
+                    if (name != null) names.add(name);
+                }
+                return names;
+            }
+            Thread.sleep(10); // 10ms 대기 후 해당 과정을 다시 반복
+        }
+        return Collections.emptyList(); // deadlock이 감지되지 않은 경우 빈 list 를 반환
+    }
+
+}
